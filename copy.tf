5346e4444270d577697c761ae129e4a9edfb79c8
samouel youssry <129524016+samouelyoussry@users.noreply.github.com>
2024-04-12 23:36:05 +0200

    Update copy.tf

--- copy.tf
+++ copy.tf
@@ -22,7 +22,7 @@ resource "azurerm_virtual_network" "app_network" {
 }
 
 resource "azurerm_subnet" "SubnetA" {
-  name                 = "SubnetA"
+  name                 = var.subnet
   resource_group_name  = local.resource_group
   virtual_network_name = azurerm_virtual_network.app_network.name
   address_prefixes     = ["10.0.1.0/24"]
