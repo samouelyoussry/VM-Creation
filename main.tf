3e838512f72409e9de6570fec9d6a564fd5d72b1
samouel youssry <129524016+samouelyoussry@users.noreply.github.com>
2024-04-13 00:45:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,14 +18,14 @@ resource "azurerm_virtual_network" "app_network" {
   name                = var.VirtualNetwork
   location            = var.location
   resource_group_name = azurerm_resource_group.terraform
-  address_space       = var.VnetAddress
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "SubnetA" {
   name                 = var.subnet
   resource_group_name  = azurerm_resource_group.terraform.name
   virtual_network_name = azurerm_virtual_network.app_network.name
-  address_prefixes     = var.VMsubnet
+  address_prefixes     = ["10.0.2.0/24"]
   depends_on           = [azurerm_virtual_network.app_network ]
 }
 
