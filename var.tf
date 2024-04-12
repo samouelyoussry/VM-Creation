c1d256b7888c26aa3ff92b22e5dedcd60bd3c295
samouel youssry <129524016+samouelyoussry@users.noreply.github.com>
2024-04-13 00:42:31 +0200

    Update var.tf

--- var.tf
+++ var.tf
@@ -13,12 +13,10 @@ variable "subnet" {
 
 variable "VnetAddress" {
   type        = list(string)
-  default     = ["10.0.0.0/16"]
 }
 
 variable "VMsubnet" {
   type        = list(string)
-  default     = ["10.0.1.0/24"]
 }
 variable "VM_interface" {
   type = string
