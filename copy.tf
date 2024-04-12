c4fd1817fdfd7b502f755e31451f4a72cdebe883
samouel youssry <129524016+samouelyoussry@users.noreply.github.com>
2024-04-12 23:46:13 +0200

    Update copy.tf

--- copy.tf
+++ copy.tf
@@ -31,21 +31,19 @@ resource "azurerm_subnet" "SubnetA" {
   ]
 }
 
-resource "azurerm_network_interface" "app_interface" {
-  name                = "app-interface"
-  location            = local.location
-  resource_group_name = local.resource_group
+resource "azurerm_network_interface" "VM_interface" {
+  name                = var.VM_interface
+  location            = var.location
+  resource_group_name = azurerm_resource_group.terraform
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.SubnetA.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.app_public_ip.id
   }
 
   depends_on = [
-    azurerm_virtual_network.app_network,
-    azurerm_public_ip.app_public_ip
+    azurerm_virtual_network.app_network
   ]
 }
 
