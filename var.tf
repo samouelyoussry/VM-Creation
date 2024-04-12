e6d8d26f9a1a2873441a071ace426fdb36879288
samouel youssry <129524016+samouelyoussry@users.noreply.github.com>
2024-04-12 23:26:19 +0200

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,1 +1,19 @@
 
+variable "RGName" {
+  type = string
+}
+variable "location" {
+  type = string 
+}
+variable "subscriptionID" {
+  type = string
+}
+variable "clientID" {
+  type = string  
+}
+variable "clientSecret" {
+  type = string  
+}
+variable "tenantID" {
+  type = string
+}
