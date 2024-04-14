3187be517368013f460fcc8b997f45057ce9e561
samouel youssry <129524016+samouelyoussry@users.noreply.github.com>
2024-04-14 11:31:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 provider "azurerm" {
-  version = "=2.0.0"
-}
+  version = "<=2.0.0"
 
+  subscription_id=var.subscriptionID
+  client_id = var.clientID
+  client_secret = var.clientSecret
+  tenant_id = var.tenantID
+  features {}
+}
 resource "azurerm_resource_group" "test1_rg" {
   name     = var.test1_resource_group_name
   location = var.location
