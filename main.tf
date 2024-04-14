e63b4920d384f1ac300b47edefc2887fc82c03db
samouel youssry <129524016+samouelyoussry@users.noreply.github.com>
2024-04-14 11:47:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,8 +101,8 @@ resource "azurerm_network_security_group" "nsg_subnet_b" {
 resource "azurerm_virtual_machine" "app_vm" {
   count                = 2
   name                 = var.app_vm_name[count.index]
-  size                 = var.VMSize
   location             = azurerm_resource_group.test1_rg.location
+  size                 = var.VMSize
   resource_group_name  = azurerm_resource_group.test1_rg.name
   network_interface_ids = [azurerm_network_interface.app_nic[count.index].id]
 
