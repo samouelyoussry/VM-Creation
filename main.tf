830362c5a6eae9656af6eafa899408b50814c218
samouel youssry <129524016+samouelyoussry@users.noreply.github.com>
2024-04-14 11:42:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_subnet" "subnet_a" {
   name                 = var.subnet_a_name
   resource_group_name  = azurerm_resource_group.test1_rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.subnet_a_address_prefix
+  address_prefix       = var.subnet_a_address_prefix
   depends_on           = [azurerm_virtual_network.vnet]
 }
 
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "subnet_b" {
   name                 = var.subnet_b_name
   resource_group_name  = azurerm_resource_group.test1_rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.subnet_b_address_prefix
+  address_prefix       = var.subnet_b_address_prefix
   depends_on           = [azurerm_virtual_network.vnet]
 }
 
