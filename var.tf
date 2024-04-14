a61416fc171613cb19cb18fb68e7726be16c8646
samouel youssry <129524016+samouelyoussry@users.noreply.github.com>
2024-04-14 10:58:01 +0200

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,43 +1,89 @@
-
-variable "RGName" {
-  type = string
-}
-
-variable "VirtualNetwork" {
-  type = string
-}
-
-variable "subnet" {
-  type = string
-}
-
-variable "VMSubnet" {
-  type = string
-}
-
-variable "VM_interface" {
-  type = string
-}
-
-variable "vmname" {
-  type = string
-}
-
-variable "publicIP" {
-  type = string
-}
 variable "location" {
-  type = string 
+  description = "The location/region where the resources will be created."
+  type        = string
+  default     = "eastus"
 }
-variable "subscriptionID" {
-  type = string
+
+variable "test1_resource_group_name" {
+  description = "Name of the test1 resource group."
+  type        = string
+  default     = "test1"
 }
-variable "clientID" {
-  type = string  
+
+variable "test2_resource_group_name" {
+  description = "Name of the test2 resource group."
+  type        = string
+  default     = "test2"
 }
-variable "clientSecret" {
-  type = string  
+
+variable "virtual_network_name" {
+  description = "Name of the virtual network."
+  type        = string
+  default     = "test-vnet"
 }
-variable "tenantID" {
-  type = string
+
+variable "vnet_address_space" {
+  description = "Address space of the virtual network."
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_a_name" {
+  description = "Name of subnet A."
+  type        = string
+  default     = "subnet-a"
+}
+
+variable "subnet_b_name" {
+  description = "Name of subnet B."
+  type        = string
+  default     = "subnet-b"
+}
+
+variable "subnet_a_address_prefix" {
+  description = "Address prefix of subnet A."
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_b_address_prefix" {
+  description = "Address prefix of subnet B."
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "app_vm_name" {
+  description = "Names of the application virtual machines."
+  type        = list(string)
+  default     = ["app-vm-1", "app-vm-2"]
+}
+
+variable "app_public_ip_name" {
+  description = "Name of the public IP attached to the app VM."
+  type        = string
+  default     = "app-public-IP"
+}
+
+variable "nsg_subnet_a_name" {
+  description = "Name of the NSG attached to subnet A."
+  type        = string
+  default     = "nsg-subnet-a"
+}
+
+variable "nsg_subnet_b_name" {
+  description = "Name of the NSG attached to subnet B."
+  type        = string
+  default     = "nsg-subnet-b"
+}
+
+variable "admin_username" {
+  description = "Admin username for the virtual machines."
+  type        = string
+  default     = "adminuser"
+}
+
+variable "admin_password" {
+  description = "Admin password for the virtual machines."
+  type        = string
+  default     = "AdminPa$$w0rd"
 }
